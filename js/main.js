f748bb84249ddb355f902291d8e1895cc5cb3658
skadbsdk <64511505+skadbsdk@users.noreply.github.com>
2020-05-07 16:05:36 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function main(){
+    skip();
+      banner();
+      login();
+      popup();
+      sitemap();
+      subPosition();
+      $(window).resize(function(){
+        subPosition();
+      })
+
+      // swiper
+      var swiper = new Swiper('.swiper-container',{
+        slidesPerView:3.5,
+        spaceBetween: 40,
+        loop:true,       
+      });
+
+
+      $('.git_hub').click(function(){
+        location.href='https://github.com/skadbsdk/megabox';
+      });
+
+      var cnt=0;
+      $('#prev').click(function(){
+        if(cnt>0){
+          cnt--;
+        }else{
+          cnt=3;
+        }       
+        userSlide(cnt);
+      })
+      $('#next').click(function(){
+        if(cnt<3){
+          cnt++;
+        }else{
+          cnt=0;
+        }
+        userSlide(cnt);
+      })
+}
