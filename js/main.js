dd6c891005a6ae695c1ccf82e1f08ae572df6639
skadbsdk <64511505+skadbsdk@users.noreply.github.com>
2020-05-08 09:56:53 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,41 +1,41 @@
 function main(){
-    skip();
-      banner();
-      login();
-      popup();
-      sitemap();
-      subPosition();
-      $(window).resize(function(){
-        subPosition();
-      })
+  skip();
+  banner();
+  login();
+  popup();
+  sitemap();
+  subPosition();
+  $(window).resize(function(){
+    subPosition();
+  })
 
-      // swiper
-      var swiper = new Swiper('.swiper-container',{
-        slidesPerView:3.5,
-        spaceBetween: 40,
-        loop:true,       
-      });
+  // swiper
+  var swiper = new Swiper('.swiper-container',{
+    slidesPerView:3.5,
+    spaceBetween: 40,
+    loop:true,       
+  });
 
 
-      $('.git_hub').click(function(){
-        location.href='https://github.com/skadbsdk/megabox';
-      });
+  $('.git_hub').click(function(){
+    location.href='https://github.com/pam7462/megabox';
+  });
 
-      var cnt=0;
-      $('#prev').click(function(){
-        if(cnt>0){
-          cnt--;
-        }else{
-          cnt=3;
-        }       
-        userSlide(cnt);
-      })
-      $('#next').click(function(){
-        if(cnt<3){
-          cnt++;
-        }else{
-          cnt=0;
-        }
-        userSlide(cnt);
-      })
+  var cnt=0;
+  $('#prev').click(function(){
+    if(cnt>0){
+      cnt--;
+    }else{
+      cnt=3;
+    }       
+    userSlide(cnt);
+  })
+  $('#next').click(function(){
+    if(cnt<3){
+      cnt++;
+    }else{
+      cnt=0;
+    }
+    userSlide(cnt);
+  })
 }
